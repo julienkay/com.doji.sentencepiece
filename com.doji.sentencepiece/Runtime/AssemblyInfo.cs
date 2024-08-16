dd693f9c213d03ff5b28e6aed92383b196e00e6f
Julien Kipp <julien@doji-tech.com>
2024-08-16 18:05:47 +0200

    initial release

---
+++ com.doji.sentencepiece/Runtime/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("Doji.Sentencepiece")]
+[assembly: AssemblyDescription("A Unity package for SentencePiece tokenization.")]
+[assembly: AssemblyCompany("Doji Technologies")]
+[assembly: AssemblyProduct("Doji.Sentencepiece")]
+[assembly: AssemblyCopyright("Copyright © Julien Kipp 2024")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0")]
